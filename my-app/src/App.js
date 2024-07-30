8b1ac5e22b3f7bdaaa03a5e790c3d7aa43ab90b6
Victor <victorjaber19@gmail.com>
2024-07-30 11:52:12 -0300

    Testing CI/CD

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Gabriel Broslavschi.
+          Edit <code>src/App.js</code> CI/CD Feito com Sucesso.
         </p>
         <a
           className="App-link"
