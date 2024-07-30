ba7e1133980eaace75e56247fa5c6e6dc2e03db2
Victor <victorjaber19@gmail.com>
2024-07-30 11:42:15 -0300

    Testing CI/CD

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> TESTANDO MUDANÇAS.
+          Edit <code>src/App.js</code> Gabriel Broslavschi.
         </p>
         <a
           className="App-link"
